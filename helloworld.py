de316d6d25883476016533b131dd889ef4cbde72
anaydurgude <anaydurgude@gmail.com>
2026-03-31 13:04:41 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 12:33:02 2026
+
+@author: 2007a
+"""
+
+def PrintHelloWorld():
+    #this function prints hello world
+    print("Hello World")
+    
+def main():
+    #this is the main function of the program
+    PrintHelloWorld()
+    
+if __name__ == "__main__":
+    main()
